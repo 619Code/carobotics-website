3d50e13a01f8b78707ecd68a0f730ffe6822a899
Peter Edwards <peter.d.edwards@gmail.com>
2026-05-25 23:11:13 -0400

    layoug and hero changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { loadEnv } from 'vite';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+
+const env = loadEnv(process.env.NODE_ENV ?? 'development', process.cwd(), '');
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://www.carobotics.org',
-	base: '/carobotics-website',
+	base: env.BASE_PATH || '/',
 	integrations: [mdx(), sitemap()],
 });
